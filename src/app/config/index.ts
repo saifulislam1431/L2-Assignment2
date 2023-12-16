9bc4c057b4ff019c6af5e7dd0096de728fd6af90
Saiful Islam <sakibbhaiya143@gmail.com>
2023-12-16 19:16:48 +0600

    Server configure with DB

---
+++ src/app/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join((process.cwd(), ".env")) });
+
+export default {
+  port: process.env.PORT,
+  database_url: `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@cluster0.8cnv71c.mongodb.net/?retryWrites=true&w=majority`,
+  bcrypt_sal_rounds: process.env.BCRYPT_SALT_ROUNDS,
+};
